c9db58a4e7b2f0fd001aa76b442e1b2325a0679a
KAP <KAP@DESKTOP-DNB9NIU>
2021-09-14 16:24:01 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Portfolio
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) => WindowState = WindowState.Maximized;
+
+        private void BtnLeave(object sender, MouseEventArgs e) => ((Button) sender).Background = null;
+        private void BtnMove(object sender, MouseEventArgs e) => ((Button)sender).Background = new SolidColorBrush(Color.FromArgb(20,200,200,200));
+    }
+}
